60b10a9bac85169eb17c7deea59f7d3bac4e4e58
wendy97531234 <126539753+wendy97531234@users.noreply.github.com>
2024-09-10 09:08:01 -0600

    sesion 3

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using   System;
+
+namespace Arrays
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Crea un programa que defina un arreglo de enteros con 5 números,
+            //  luego suma todos los elementos del arreglo e imprime el resultado en pantalla.
+
+            int[] numeros = { 2, 4, 6, 8, 10 };
+            int suma = 0;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+            suma += numeros[i];
+            }
+            Console.WriteLine("La suma de los elementos es:" + suma); 
+
+
+        }
+    }
+}
---
+++ Arrays/List/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace List
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crea un programa que permita al usuario agregar nombres a una lista hasta que
+            //ingrese "salir". Luego imprime todos los nombres ingresados.
+
+            List<string> nombres = new List<string>();
+            string nombre;
+
+            do
+            {
+                Console.WriteLine("Ingrese un nombre o escriba 'salir' para terminar");
+                nombre = Console.ReadLine();
+
+                if (nombre.ToLower () != "Salir")
+                {
+                    nombres.Add(nombre);
+                }
+
+            } while (nombre.ToLower() != "salir");
+
+            Console.WriteLine("Nombres ingresados");
+
+            foreach (string n in nombres)
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+    }
+}
